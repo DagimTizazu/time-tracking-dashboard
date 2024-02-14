f394e4ecae126cbbd73f6074f4629a979162abc9
Dagim Tizazu <dagimtizazu4444@gmail.com>
2024-02-14 12:27:50 -0900

    Update Card.jsx
    fixed heading levels mistake

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,11 +11,11 @@ function Card({ type, current, backgroundcolor, backgroundimage, previous, frame
 
       <div className="inner-card-container">
 
-        <h3 className='card-type'>{type}</h3>
+        <h2 className='card-type'>{type}</h2>
           <svg className='menu-dots' width="21" height="5"  xmlns="http://www.w3.org/2000/svg">
                 <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"  fill-rule="evenodd" />
           </svg>
-        <h2 className='card-current'>{current}{'hrs'}</h2>
+        <h3 className='card-current'>{current}{'hrs'}</h3>
           
         <p className='card-previous'>Last {frame} - {previous}{'hrs'}</p>
         
