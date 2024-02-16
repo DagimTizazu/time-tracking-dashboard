cdfccfaf1e0b9d0f5b5ac3c2dfc749c69e9c5e6f
Dagim Tizazu <dagimtizazu4444@gmail.com>
2024-02-16 03:54:16 -0900

    Revert "Update app.jsx"
    This reverts commit 8296fa7f9bde8ed7c555c2159da9d066af230ece.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -64,7 +64,7 @@ function App() {
         <Card key={i}
           type={data.title}
           backgroundcolor={backgroundColors[i]}
-          backgroundimage={backgroundImages[i] + ' !important'}
+          backgroundimage={backgroundImages[i]}
           current={
             timeFrame === 'daily' ? data.timeframes.daily.current :
               timeFrame === 'weekly' ? data.timeframes.weekly.current :
